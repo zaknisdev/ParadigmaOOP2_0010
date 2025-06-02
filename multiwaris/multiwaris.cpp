764cf8cd232bd364e2e42ec56023b1adcf121205
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:25:35 +0700

    membuang anggota orang dengan umur

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -4,6 +4,10 @@ using namespace std;
 class orang {
     public: 
         int umur;
-
         
-}
+        orang(int pUmur) :
+            umur (pUmur)
+            {
+                cout << "Orang dibuat dengan umur" << umur << "\n" << endl;
+            }
+};
