399f6ed6996a0c5268b12f9ef39e3bda49d6a2d0
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:08:54 +0700

    membuat variabel untuk memasukkan variabel i sama dengan variabel di saklarNo

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -8,4 +8,7 @@ class remoteLampu {
         void setSaklarNo(int i, string value) {
             saklarNo[i] = value;
         }
+        string getSaklarNo(int i) {
+            return saklarNo[i];
+        }
 };
