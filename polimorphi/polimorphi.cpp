7bde9d0dbd9d14391842cf408ae0ceb2fd1710d8
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 11:08:35 +0700

    membuat class sesorang dengan member virtual function

--- polimorphi/polimorphi.cpp
+++ polimorphi/polimorphi.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+class seseorang {
+    virtual void pesan() = 0
+    // virtual void pesan() {
+    //      cout << "Pesan dari sesorang" << endl;
+    // }
+};
+
