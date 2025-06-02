d6c7fb2a426f38c40ee3400d2cf97e9de96fa1a0
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:48:37 +0700

    membuat objek derivedClass dan memanggil function perkenalan melalui objek

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -14,3 +14,10 @@ class derivedClass : public baseClass {
             cout << "Hallo saya Function dari derived Class";
         }
 };
+
+int main() {
+    derivedClass a;
+
+    a.perkenalan();
+    return 0;
+}
