54e645a6dd89ae40d0b19a42be29cd83b710606d
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:07:40 +0700

    membuat method  saklarNo sesuai dengan value variabel i

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -4,4 +4,8 @@ using namespace std;
 class remoteLampu {
     private:
         string saklarNo[10];
+    public:
+        void setSaklarNo(int i, string value) {
+            saklarNo[i] = value;
+        }
 };
