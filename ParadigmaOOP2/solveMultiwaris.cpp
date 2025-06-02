d3f1df03ba3f8f73410bdd6b967d8b2080a1f377
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 11:04:09 +0700

    membuat class orang, pekerja, pelajar, budi

--- ParadigmaOOP2/solveMultiwaris.cpp
+++ ParadigmaOOP2/solveMultiwaris.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+    public: 
+        int umur;
+        
+        orang(int pUmur) :
+            umur (pUmur)
+            {
+                cout << "Orang dibuat dengan umur" << umur << "\n" << endl;
+            }
+};
+
+class pekerja : public orang {
+    public:
+	pekerja(int pUmur) : orang(pUmur)
+	{
+	cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : public orang {
+    public: 
+        pelajar(int pUmur) : orang(pUmur)
+        {
+            cout << " pelajar dibuat\n" << endl;
+        }
+};
+
+class budi : public pekerja, public pelajar {
+    public:
+        budi(int pUmur) : pekerja(pUmur), pelajar(pUmur)
+        {
+            cout << "Budi dibuat\n" << endl;
+        }
+};
+
