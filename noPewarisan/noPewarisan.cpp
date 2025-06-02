71cf1d79c7e0cafca6d81972c3e405e0c07e842f
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:51:19 +0700

    membuat cclass baseclass dengan member virtual function

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+class baseClass {
+    public:
+        virtual void perkenalan() {
+            cout << "Hallo saya Function dari base class";
+        }
+};
