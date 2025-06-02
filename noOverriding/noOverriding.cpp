687b37f94f657ba59fba1dedb3132d2ea132ac28
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 10:55:53 +0700

    menambahkan final ada function perkenalan

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class baseClass {
     public:
-        virtual void perkenalan() {
+        virtual void perkenalan() final{
             cout << "Hallo saya Function dari base class";
         }
 };
