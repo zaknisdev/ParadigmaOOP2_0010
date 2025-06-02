90dbde15400c0b980dc52d589bdb06bb8fb36728
zaknisdev <zakianisf1710@gmail.com>
2025-06-02 11:15:09 +0700

    memanggil function melalui objek

--- polimorphi/polimorphi.cpp
+++ polimorphi/polimorphi.cpp
@@ -27,6 +27,13 @@ int main() {
     seseorang* obyek;
     joko a;
     lia b;
-    
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
 }
 
